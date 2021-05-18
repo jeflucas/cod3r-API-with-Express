ac2fc3ee89f9da98d8ff8329d95a8d63419e15a7
jeflucas <jef.lucas@hotmail.co.uk>
2021-05-17 23:51:03 -0300

    aula

--- src/servidor.js
+++ src/servidor.js
@@ -3,10 +3,15 @@ const porta = 3003
 const express = require('express')
 const app = express()
 
-app.get('./produtos', (req, res, next) =>{
+app.get('/produtos', (req, res, next) =>{
+    res.send({ nome: 'Notebook', preco: 123.45}) //vai converter para JSON
+})
+
+
+app.get('/produtos', (req, res, next) =>{
     res.send({ nome: 'Notebook', preco: 123.45}) //vai converter para JSON
 })
 
 app.listen(porta, () =>{
-    console.log(`Servidor executando na porta ${porta}`)
+    console.log(`Servidor está executando na porta ${porta}`)
 })
